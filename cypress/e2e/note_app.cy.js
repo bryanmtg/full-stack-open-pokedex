24cd2f2e1ea469b0431d0b1026054defd4d2eb09
Bryan Zhang <bryan@lands.design>
2022-08-25 19:09:04 -0700

    added cypress tests

---
+++ cypress/e2e/note_app.cy.js
@@ -0,0 +1,12 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('can navigate to a particular Pokemon page', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('a[href*="pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
+  })
+})
